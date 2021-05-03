50f08c767a143df83fabefda148e7fdc80151731
harappa <bananation712@gmail.com>
2021-05-04 01:20:21 +0900

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 session_start();
 
-define('DSN', 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_475bc4d54b850;charset=utf8mb4');
-define('DB_USER', 'b073d324f2ec96');
-define('DB_PASS', 'b4a1a844');
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 spl_autoload_register(function ($class) {
